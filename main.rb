9ed90f18ceb8252686a977b9dd4c81ddd71f2279
Meenakshi <abhi@codeanand.com>
2019-12-29 17:29:37 -0800

    initial database setup done

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
 require_relative 'framework'
+require_relative 'db'
+require_relative 'queries'
+
+DB = Databse.connect('postgres://localhost/framework_dev',QUERIES)
 
 APP = App.new do
 
@@ -9,5 +13,18 @@ APP = App.new do
   get '/users/:username' do |params|
     "This is a #{params.fetch('username')}"
   end
+  get '/submissions' do |params|
+    # DB.exec_sql('select * from submissions;')
+    DB.all_submissions
 
+
+  end
+
+  get '/submissions/:name' do |params|
+    name = params.fetch('name')
+    #  DB.exec_sql('select * from submissions where name =')
+    user = DB.find_submissions_by_name(name,params).fetch(0)
+    "The user is #{user.fetch('name')}"
+
+  end
 end
---
+++ queries.rb
@@ -0,0 +1,9 @@
+QUERIES = {
+  all_submissions: %{
+    select * from submissions;
+  },
+  find_submissions_by_name: %{
+    select * from submissions
+    where name = '%s';
+  }
+}
